5191f7bad4267ff0a540c3d9c28bfc72b4d8c954
paulinaczy <73669289+paulinaczy@users.noreply.github.com>
2020-11-25 16:10:26 +0100

    Update transportujFlota.hpp

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -1,9 +1,23 @@
 #pragma once
 
 #include "Stocznia.hpp"
-
 unsigned int transportujFlota(unsigned int towar)
+{unsigned int liczba_z = 0;
+unsigned int suma_t = 0;
+
+if (towar == 0)
+{ return 0;}
+else (
+while (suma_t<towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+Stocznia stocznia{};
+Statek* s1=stocznia();
+suma_t=suma_t+s1->transportuj();
+if (s1==dynamic_cast<Zaglowiec*>(s1))
+{
+Zaglowiec=Zaglowiec+1;
+}
+delete s1;
+}
+return liczba_z;
 }
